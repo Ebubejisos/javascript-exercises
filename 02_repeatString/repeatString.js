414f4b9b54373047ced79ac9293dfd7b9551ace0
Ebubejisos <anosikegideon007@gmail.com>
2022-10-06 16:51:06 +0100

    solved the first two code tests
    added the required codes in helloWorld.js and repeatString.js in other to pass the tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,12 @@
-const repeatString = function() {
-
-};
-
+const repeatString = (string, num) => {
+    let word = "";
+    if(num < 0){
+        return 'ERROR';
+    }
+    for(let i=0; i<num; i++){
+        word += string;
+    };
+    return word;
+}
 // Do not edit below this line
 module.exports = repeatString;
