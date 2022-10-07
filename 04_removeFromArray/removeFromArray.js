baf055ecb0398ffb6cf412ad5ddb982ec2bba7d6
Ebubejisos <anosikegideon007@gmail.com>
2022-10-07 20:07:27 +0100

    completed the tests of 4 files
    wrote the codes which passed the test number 04 to 05

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(...args) {
+    let array = arguments[0];
+    let newArray = [];
+    for (let i=0; i < array.length; i++) {
+        if (!args.includes(array[i])) {
+            newArray.push(array[i]);
+        }
+    };
+    return newArray;
 
 };
-
 // Do not edit below this line
 module.exports = removeFromArray;
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(min, max) {
+    let sumEl = 0
+    if(min > max && max > 0 && typeof min === "number" && typeof max === "number"){
+        for(let i = max; i <= min; i++){
+            sumEl += i;
+        }
+    }
+    else if(min < max && min > 0 && typeof min === "number" && typeof max === "number"){
+        for(let i = min; i <= max; i++){
+        sumEl += i;
+        }
+    }
+    else{ return "ERROR"}
+    return sumEl
 };
 
 // Do not edit below this line
