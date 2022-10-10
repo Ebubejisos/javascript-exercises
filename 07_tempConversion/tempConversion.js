a00761da06aa1d0d1925f1fa0cfda711b4a2593d
Ebubejisos <anosikegideon007@gmail.com>
2022-10-11 00:11:48 +0100

    completed Jest 07 and 08
    wrote the correct codes to pass the tests in 07 and 08

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(F) {
+  let celsuis = (5 * (F - 32)) / 9;
+  celsuis = Math.round((celsuis + Number.EPSILON) * 10) / 10;
+  return celsuis;
 };
 
-const ctof = function() {
-
+const ctof = function(C) {
+  let fahrenheit = ((C * 9) / 5) + 32;
+  fahrenheit = Math.round((fahrenheit + Number.EPSILON) * 10) / 10;
+  return fahrenheit;
 };
 
 // Do not edit below this line
--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,45 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a - b;
 };
 
 const sum = function() {
-	
+	let array = arguments[0];
+  let sum = 0;
+  for (let nums of array){
+    sum += nums;
+  }
+  return sum;
 };
 
 const multiply = function() {
-
+  let array = arguments[0];
+  let multiple = 1;
+  for (let nums of array){
+    multiple *= nums;
+  }
+  return multiple;
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return Math.pow(a,b);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let product = 1
+  if(num < 2){
+    return product;
+  }
+  else{
+    for(i = num; i >= 1; i--){
+      product *= i;
+    }
+  }
+  product = Math.round((product + Number.EPSILON) * 100) / 100;//rounds up to 2decimal places if necessary
+  return product;
 };
 
 // Do not edit below this line
