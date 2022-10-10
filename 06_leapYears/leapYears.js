fe5e44d201298551b8d6c65df8f83f1836998880
Ebubejisos <anosikegideon007@gmail.com>
2022-10-10 22:40:19 +0100

    completed leapYear.js test
    edited the code in leapYear.js to pass the tests in the spec.js file

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,18 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    let isLeapYear = true;
+    const four = year % 4 == 0 ? true : false;
+    const fourHundred = year % 400 == 0 ? true : false;
+    const hundred = year % 100 == 0 ? true : false;
+    if(four == true && hundred !== true){
+        isLeapYear;
+    }
+    else if(fourHundred == true){
+        isLeapYear;
+    }
+    else{isLeapYear = false};
+    return isLeapYear;
 };
+console.log(leapYears(1800))
 
 // Do not edit below this line
 module.exports = leapYears;
