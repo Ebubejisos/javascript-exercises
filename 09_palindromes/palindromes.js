c3ff0a3ef669a09a6eb4e7e4413dadeca7cce5b3
Ebubejisos <anosikegideon007@gmail.com>
2022-11-03 15:43:34 +0100

    solved more jests
    completed 09, 10 and 11 jest

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,18 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    let isPalindrome = false;
 
+    let lowercaseString = string.toLowerCase();
+    let punctionlessString = lowercaseString.replace(/[\.,-\/#!$%\^&\*;:{}=\-_`~()@\+\?><\[\]\+]/g, ''); 
+    let spacelessString = punctionlessString.replace(/\s{1,}/g, '');
+    //above regex replaces whitespace(\s) greater than 1({1,}) with no space('')
+    let stringArray = spacelessString.split('');
+    let reversedArray = stringArray.reverse().join('');
+    
+    if(reversedArray == spacelessString){
+        isPalindrome = true;
+    }
+    
+    return isPalindrome;
 };
-
 // Do not edit below this line
 module.exports = palindromes;
--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function(index) {
+    if(index < 0){
+        return 'OOPS';
+    }
+    let fib = [1,1];
+    for(let i = 1;i <= 25;i++){
+        let nextNum = fib[i] + fib[i-1];
+        fib.push(nextNum);
+    }
+    return fib[index - 1];
 };
 
 // Do not edit below this line
